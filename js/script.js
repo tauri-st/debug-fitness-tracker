c1ce6186fb42848880ce1d006008c94342250c48
tauri_s <tauristclaire@gmail.com>
2022-08-12 16:44:29 -0700

    Initial commit with buggy code

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const person = {
+  name: "Alice",
+  lastName = "Parker",
+  student: True
+}
+
+console.log(Alice.student);
+
+console.log(person.lastName);
+
+console.log(person.name);
+
+person.totalSteps = 200;
+
+person.walk = function(steps){
+  totalSteps += steps;
+}
+
+person.walk(250);
+
+console.log(person.totalsteps);
